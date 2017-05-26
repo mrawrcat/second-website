2e2548ddad1aa5787f949ab08182d0096d69ba8d
graceabby <ace_e303@e303-s25.local>
2017-05-26 12:44:56 -0400

    made a contact page

--- js/sidenavpush.js
+++ js/sidenavpush.js
@@ -1,7 +1,7 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidenav").style.width = "150px";
+    document.getElementById("main").style.marginLeft = "150px";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
